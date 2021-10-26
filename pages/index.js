2018c1b2230946948568e219ba5ac1b5e6b5e79f
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2021-10-26 13:19:33 +0530

    Building the middle section of Header

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Instagram 2.0</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div>
+			<Head>
+				<title>Instagram 2.0</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 
-     <h1>Insta clone</h1>
-    </div>
-  )
+			{/* <h1>Insta clone</h1> */}
+      <Header />
+		</div>
+	);
 }
