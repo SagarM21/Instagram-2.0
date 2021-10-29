6d12a6a64450b7add5de228263d010e0f39a38a8
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2021-10-29 16:55:11 +0530

    Frontend: Upload photo in Post Section done

--- components/Posts.js
+++ components/Posts.js
@@ -1,33 +1,32 @@
+import { collection, onSnapshot, orderBy, query } from "@firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
 import Post from "./Post";
 
-const posts = [
-	{
-		id: "123",
-		username: "sagar_maheshh",
-		userImg: "https://links.papareact.com/3ke",
-		img: "https://links.papareact.com/3ke",
-		caption: "Insta clone in making",
-	},
-	{
-		id: "1233",
-		username: "sagar_maheshh",
-		userImg: "https://links.papareact.com/3ke",
-		img: "https://links.papareact.com/3ke",
-		caption: "Insta clone in making",
-	},
-];
-
 function Posts() {
+	const [posts, setPosts] = useState([]);
+	useEffect(
+		() =>
+			onSnapshot(
+				query(collection(db, "posts"), orderBy("timestamp", "desc")),
+				(snapshot) => {
+					setPosts(snapshot.docs);
+				}
+			),
+
+		[db]
+	);
+
 	return (
 		<div>
 			{posts.map((post) => (
 				<Post
 					key={post.id}
 					id={post.id}
-					username={post.username}
-					userImg={post.userImg}
-					img={post.img}
-					caption={post.caption}
+					username={post.data().username}
+					userImg={post.data().profileImg}
+					img={post.data().image}
+					caption={post.data().caption}
 				/>
 			))}
 		</div>
